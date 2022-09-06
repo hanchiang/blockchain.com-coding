b0c45adfbc431a40d0982d8fb44b8d3bee4240a1
han chiang <yaphc@hotmail.com>
2022-09-07 07:35:42 +0800

    Rename dependencies for better clarity

--- src/dependencies/dependencies.py
+++ src/dependencies/dependencies.py
@@ -16,10 +16,10 @@ from src.service.csv_metadata_parser import CsvMetadataParser
 
 
 class Dependencies:
-    order_book_service: dict[str, ThirdPartyOrderBookService] = {}
-    order_book_transformer: dict[str, ThirdPartyOrderBookTransformer] = {}
-    metadata_dao: dict[str, MetadataDao] = {}
-    metadata_parser: dict[str, MetadataParser] = {}
+    order_book_service_by_exchange: dict[str, ThirdPartyOrderBookService] = {}
+    order_book_transformer_by_exchange: dict[str, ThirdPartyOrderBookTransformer] = {}
+    metadata_dao_by_storage: dict[str, MetadataDao] = {}
+    metadata_parser_by_file_type: dict[str, MetadataParser] = {}
     is_initialised = False
 
     @staticmethod
@@ -33,7 +33,7 @@ class Dependencies:
 
     @staticmethod
     def build_order_book_service():
-        Dependencies.order_book_service = {
+        Dependencies.order_book_service_by_exchange = {
             "blockchain.com": BlockChainOrderBookService(
                 base_url=Config.get_env_var("BLOCKCHAIN_BASE_URL")
             )
@@ -41,22 +41,22 @@ class Dependencies:
 
     @staticmethod
     def build_order_book_transformer():
-        Dependencies.order_book_transformer = {
+        Dependencies.order_book_transformer_by_exchange = {
             "blockchain.com": BlockChainOrderBookTransformer()
         }
 
     @staticmethod
     def build_order_book_metadata_dao():
-        Dependencies.metadata_dao = {"memory": MetadataMemoryDao()}
+        Dependencies.metadata_dao_by_storage = {"memory": MetadataMemoryDao()}
 
     @staticmethod
     def build_order_book_metadata_parser():
-        Dependencies.metadata_parser = {"csv": CsvMetadataParser()}
+        Dependencies.metadata_parser_by_file_type = {"csv": CsvMetadataParser()}
 
     @staticmethod
     def get_order_book_service(key: str) -> ThirdPartyOrderBookService:
         Dependencies.build()
-        service = Dependencies.order_book_service.get(key, None)
+        service = Dependencies.order_book_service_by_exchange.get(key, None)
         if service is None:
             raise DependencyNotFoundException(
                 f"Unable to find order book service for {key}"
@@ -66,7 +66,7 @@ class Dependencies:
     @staticmethod
     def get_order_book_transformer(key: str) -> ThirdPartyOrderBookTransformer:
         Dependencies.build()
-        transformer = Dependencies.order_book_transformer.get(key, None)
+        transformer = Dependencies.order_book_transformer_by_exchange.get(key, None)
         if transformer is None:
             raise DependencyNotFoundException(
                 f"Unable to find order book transformer for {key}"
@@ -76,7 +76,7 @@ class Dependencies:
     @staticmethod
     def get_metadata_dao(key: str) -> MetadataDao:
         Dependencies.build()
-        res = Dependencies.metadata_dao.get(key, None)
+        res = Dependencies.metadata_dao_by_storage.get(key, None)
         if res is None:
             raise DependencyNotFoundException(f"Unable to find metadata dao for {key}")
         return res
@@ -84,7 +84,7 @@ class Dependencies:
     @staticmethod
     def get_metadata_parser(key: str) -> MetadataParser:
         Dependencies.build()
-        res = Dependencies.metadata_parser.get(key, None)
+        res = Dependencies.metadata_parser_by_file_type.get(key, None)
         if res is None:
             raise DependencyNotFoundException(
                 f"Unable to find metadata reader for {key}"
